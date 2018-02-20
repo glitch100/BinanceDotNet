bf0e9554460abe83e36bc6136d323d829c5ec66f
Paulo Henrique Abreu (Take.net) <pauloh@takenet.com.br>
2018-02-20 13:03:49 -0300

    Add StringDecimalConverter to fix issue #40

---
+++ BinanceExchange.API/Converter/StringDecimalConverter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BinanceExchange.API.Converter
+{
+    public class StringDecimalConverter : JsonConverter
+    {
+        public override bool CanRead
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(decimal) || objectType == typeof(decimal?);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((decimal)value).ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
