19326d1da6a9f4663d16c5ab7817ddb98ed7f78c
BZ-CO <mcorplab@gmail.com>
2018-02-23 19:34:59 +0200

    Added combined and partial websocket methods. (#46)
    * Added combined and partial websocket methods.
    * Added missing DataContract on BinancePartialData.

---
+++ BinanceExchange.API/Models/WebSocket/BinanceDataCombined.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using BinanceExchange.API.Converter;
+using BinanceExchange.API.Models.Response;
+using BinanceExchange.API.Models.WebSocket.Interfaces;
+using Newtonsoft.Json;
+
+namespace BinanceExchange.API.Models.WebSocket
+{
+    [DataContract]
+    public class BinanceDataCombined : IWebSocketResponse
+    {
+        [DataMember(Order = 1)]
+        [JsonProperty(PropertyName = "e")]
+        public string EventType { get; set; }
+
+        [DataMember(Order = 2)]
+        [JsonProperty(PropertyName = "E")]
+        [JsonConverter(typeof(EpochTimeConverter))]
+        public DateTime EventTime { get; set; }
+
+        [DataMember(Order = 3)]
+        [JsonProperty(PropertyName = "s")]
+        public string Symbol { get; set; }
+
+        [DataMember(Order = 4)]
+        [JsonProperty(PropertyName = "u")]
+        public long UpdateId { get; set; }
+
+        [DataMember(Order = 5)]
+        [JsonProperty(PropertyName = "b")]
+        [JsonConverter(typeof(TraderPriceConverter))]
+        public List<TradeResponse> BidDepthDeltas { get; set; }
+
+        [DataMember(Order = 6)]
+        [JsonProperty(PropertyName = "a")]
+        [JsonConverter(typeof(TraderPriceConverter))]
+        public List<TradeResponse> AskDepthDeltas { get; set; }
+    }
+}
