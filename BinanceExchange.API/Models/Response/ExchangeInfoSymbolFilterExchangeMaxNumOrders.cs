90398b46ef96bfdd751e45d528ba9bd849bc0093
Alexander <5854005+AlexN78@users.noreply.github.com>
2019-03-18 10:51:11 +0200

    Added support of some new ExchangeInfoSymbolFilterType items. (#111)
    * Added support of some new ExchangeInfoSymbolFilterType items.
    
    * Added new filters: PERCENT_PRICE, MARKET_LOT_SIZE, MAX_NUM_ICEBERG_ORDERS.  (Rest API changed on 2018-11-13)

---
+++ BinanceExchange.API/Models/Response/ExchangeInfoSymbolFilterExchangeMaxNumOrders.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace BinanceExchange.API.Models.Response
+{
+    [DataContract]
+    public class ExchangeInfoSymbolFilterExchangeMaxNumOrders : ExchangeInfoSymbolFilter
+    {
+        [DataMember(Order = 1)]
+        public int Limit { get; set; }
+    }
+}
---
+++ BinanceExchange.API/Models/Response/ExchangeInfoSymbolFilterPercentPrice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using BinanceExchange.API.Converter;
+using BinanceExchange.API.Models.Response.Interfaces;
+
+namespace BinanceExchange.API.Models.Response
+{
+   [DataContract]
+    public class ExchangeInfoSymbolFilterPercentPrice : ExchangeInfoSymbolFilter
+    {
+        [DataMember(Order = 1)]
+        public Decimal MultiplierUp { get; set; }
+
+        [DataMember(Order = 2)]
+        public Decimal MultiplierDown { get; set; }
+
+        [DataMember(Order = 3)]
+        public Decimal AvgPriceMins { get; set; }
+    }
+}
