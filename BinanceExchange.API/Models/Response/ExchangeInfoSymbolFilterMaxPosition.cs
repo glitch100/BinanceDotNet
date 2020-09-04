f46bdfd8c6161dd696e696d119cd59fc29333e5d
Gayan <gayan@redberylit.com>
2020-09-04 13:14:50 +0530

    added missing MX_POSITION filter

---
+++ BinanceExchange.API/Models/Response/ExchangeInfoSymbolFilterMaxPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace BinanceExchange.API.Models.Response
+{
+    [DataContract]
+    public class ExchangeInfoSymbolFilterMaxPosition : ExchangeInfoSymbolFilter
+    {
+        [DataMember(Order = 1)]
+        public int MaxPosition { get; set; }
+    }
+}
